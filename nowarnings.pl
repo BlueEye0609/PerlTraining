073f92b84bdf19a6bc8a98cb2bb61424de787a0e
Yejing <huang_yejing@126.com>
2018-11-06 10:40:07 +0800

    only stop warning for undef value

--- nowarnings.pl
+++ nowarnings.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/env perl
-#use strict;
-use warnings FATAL => 'all';
+use strict;
+use warnings;
 
+my $x;
 {
-   no warnings uninitialized;
+   no warnings 'uninitialized';
    print "x value : $x";
 }
